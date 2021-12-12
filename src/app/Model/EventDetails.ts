459e1c9f36d4302bd4a3e61cc944c0f4ab71be10
LetsDevelopApplications <37673876+LetsDevelopApplications@users.noreply.github.com>
2021-12-12 14:22:54 -0500

    Event Details Form validation complete.

---
+++ src/app/Model/EventDetails.ts
@@ -0,0 +1,15 @@
+
+export class EventDetails {
+
+    constructor(
+    public eventId: number,
+    public FullName: string,  
+    public Email: string,  
+    public IntrestedIn: string,
+    public Message: string
+    )	
+    {
+    
+    }
+    
+    }
--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { from } from 'rxjs';
+import { EventDetails } from '../Model/EventDetails';
 
 @Component({
   selector: 'app-event-details',
@@ -7,13 +11,36 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
+  public msg: string;
+  public submitted: boolean = false;
 
-  constructor() { }
+  public eventdetails : EventDetails = 
+  {
+    eventId:null, 
+    FullName:null,
+    Email:null,
+    IntrestedIn:null,
+    Message:null
+    
+   };
+  constructor(
+    private http: HttpClient, 
+    private router: Router,
+    private route: ActivatedRoute,
+    //private productService: ProductService
+  ) { }
 
   ngOnInit() {
   }
   onSubmit(myform: NgForm)
   {
-    
+    this.submitted = true;
+    if (myform.valid)
+    {
+      this.msg="Event booking done successfully!!";
+      this.submitted = false;
+      myform.reset();
+    }
+
   }
 }
