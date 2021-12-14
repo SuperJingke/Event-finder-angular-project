64180f2a6d6af26d8ab11133eb8ec588cf5943e8
LetsDevelopApplications <37673876+LetsDevelopApplications@users.noreply.github.com>
2021-12-13 21:48:47 -0500

    book event web service call addition.

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router,ParamMap } from '@angular/router';
 import { from } from 'rxjs';
 import { EventDetails } from '../Model/EventDetails';
+import { BookEventService } from '../service/bookeventservice';
 
 @Component({
   selector: 'app-event-details',
@@ -13,6 +14,9 @@ import { EventDetails } from '../Model/EventDetails';
 export class EventDetailsComponent implements OnInit {
   public msg: string;
   public submitted: boolean = false;
+public eventId:number;
+public Email:string;
+
 
   public eventdetails : EventDetails = 
   {
@@ -27,20 +31,38 @@ export class EventDetailsComponent implements OnInit {
     private http: HttpClient, 
     private router: Router,
     private route: ActivatedRoute,
-    //private productService: ProductService
+    private bookeventservice: BookEventService
   ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.eventId = params['eventId'];
+    });
   }
   onSubmit(myform: NgForm)
   {
     this.submitted = true;
     if (myform.valid)
     {
-      this.msg="Event booking done successfully!!";
-      this.submitted = false;
-      myform.reset();
+      this.bookeventservice.bookEvent(this.eventId,this.Email,1).subscribe((data: any) => 
+      {
+        console.log(data);
+        this.eventdetails = <EventDetails>data;
+      }, (err: any) => {  console.log(err.error.status);
+    });
+
+              //this.bookeventservice.bookEvent(this.eventId,this.Email,1);
+              this.msg = "You have successfuly booked for the event!!!";  
+              this.submitted = false;
+              myform.reset();
+              
+    } 
+    else
+    {
+          this.submitted = false;
+          this.msg = "Entry is not completed!";
     }
+  }
 
   }
-}
+
---
+++ src/app/service/bookeventService.ts
@@ -0,0 +1,36 @@
+
+ import { Injectable } from '@angular/core';
+ import { HttpClient } from '@angular/common/http';
+ import { EventDetails } from '../Model/EventDetails';
+ import { BehaviorSubject, Observable } from 'rxjs';
+ import { environment } from 'src/environments/environment';
+ 
+ 
+ @Injectable({ 
+      providedIn: 'root'
+ })
+ export class BookEventService 
+ {
+    products : Observable<EventDetails[]>;
+    private _events:BehaviorSubject<EventDetails[]>;
+    private baseUrl: string;
+    private index: number =3;
+    private datastore:{
+      prods: EventDetails[]
+    };
+
+ constructor(private httpClient: HttpClient) {
+    this.baseUrl =  environment.baseUrl;  
+    this.datastore = { prods: []};
+    this._events = <BehaviorSubject<EventDetails[]>>new BehaviorSubject([]);
+    this.products = this._events.asObservable();
+   }
+
+ public bookEvent(eventid: number,email: string,count: number) 
+  {
+    console.log('calling bookEvent');
+    
+    //return this.httpClient.get(`${this.baseUrl}event/book/${eventid}/${email}/${count}`);
+    return this.httpClient.get(`${this.baseUrl}event/book/1/k.t@test.com/1`);
+  }
+}
